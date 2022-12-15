64a8e50933f4dadf17b73eef887bd3a6cdc43b63
杰克有茶 <chenwansal1@163.com>
2022-12-15 10:58:13 +0800

    <feature> add: Sine / Modulo

--- Assets/com.frame.gengen/Sample/Sample_Cycle_Modulo.cs
+++ Assets/com.frame.gengen/Sample/Sample_Cycle_Modulo.cs
@@ -3,25 +3,27 @@ using UnityEngine;
 
 namespace JackFrame.GenGen.Sample {
 
-    public class Sample : MonoBehaviour {
+    public class Sample_Cycle_Modulo : MonoBehaviour {
+
+        [SerializeField] bool active;
+
+        [SerializeField] Vector2Int board = new Vector2Int(50, 50);
+        [SerializeField] float cycleDuration;
 
         void Awake() {
-
             Console.SetOut(new UnityTextWriter());
-
         }
 
         void OnDrawGizmos() {
 
-            // Draw Perlin
-            float c = 0;
-            Vector2Int board = new Vector2Int(20, 20);
+            if (!active) {
+                return;
+            }
+
             Vector2 size = new Vector2(0.1f, 0.1f);
             for (int x = 0; x < board.x; x += 1) {
                 for (int y = 0; y < board.y; y += 1) {
-                    float nx = (float)x / board.x;
-                    float ny = (float)y / board.y;
-                    c = GGPerlinHelper.Noise(nx, ny);
+                    float c = GGCycleHelper.Modulo(x * y, cycleDuration);
                     Gizmos.color = new Color(c, c, c, 1);
                     Vector3 pos = new Vector3(x * size.x, y * size.y, 0);
                     Gizmos.DrawCube(pos, size);
